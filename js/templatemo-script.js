d936cb34119cb180a61e8632831ccd81b9912e11
Anza <danlinhnguyen04@gmail.com>
2025-02-11 08:35:06 -0500

    all files

---
+++ js/templatemo-script.js
@@ -0,0 +1,121 @@
+/*
+
+TemplateMo 560 Astro Motion
+
+https://templatemo.com/tm-560-astro-motion
+
+*/
+
+var gallery = undefined;
+
+function closeMenu() {
+  $(".navbar-collapse").removeClass("show"); 
+}
+
+function highlightMenu(no) {
+  $(".navbar .navbar-nav > .nav-item").removeClass('selected');
+  $(".navbar .navbar-nav > .nav-item > .nav-link[data-no='" + no + "']").parent().addClass('selected');
+}
+
+function setupGallery() {
+  gallery = $('.gallery-slider').slick({
+    slidesToShow: 5,
+    slidesToScroll: 3,
+    dots: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+}
+
+function openPage(no) {
+  if(no == 2) {
+    if(gallery == undefined) {
+      setupGallery();
+    } else {
+      $('.gallery-slider').slick('unslick');
+      setupGallery();
+    }    
+  }
+
+  $('.cd-hero-slider li').hide();
+  $('.cd-hero-slider li[data-page-no="' + no + '"]')
+    .fadeIn();
+}
+
+$(window).on('load', function() {
+  $('body').addClass('loaded');
+  openPage(1);
+});
+
+jQuery(function() {
+    $('.tm-page-link').on('click', function(){
+      var pageNo = $(this).data('page-no');
+      openPage(pageNo);
+      highlightMenu(pageNo);
+    });
+
+    $(".navbar .navbar-nav > .nav-item > a.nav-link").on('click', function(e){
+      var pageNo = $(this).data('no');
+
+      openPage(pageNo);
+      highlightMenu(pageNo);
+      closeMenu();     
+    });
+
+    $("html").click(function(e) {
+      closeMenu();
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const pageLinks = document.querySelectorAll('.tm-page-link');
+
+  pageLinks.forEach(link => {
+      link.addEventListener('click', function (e) {
+          e.preventDefault();
+
+          const targetPageNo = this.getAttribute('data-no');
+          const targetSection = document.querySelector(`li[data-page-no="${targetPageNo}"]`);
+
+          if (targetSection) {
+              // Hide all sections and show the target section
+              document.querySelectorAll('li[data-page-no]').forEach(section => {
+                  section.style.display = 'none';
+              });
+
+              targetSection.style.display = 'block';
+              targetSection.scrollIntoView({ behavior: 'smooth' });
+          }
+      });
+  });
+
+  // Show the first section on page load
+  document.querySelector('li[data-page-no="1"]').style.display = 'block';
+});
